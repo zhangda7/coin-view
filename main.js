6a0993d895c4e6eaec82c17e3be16474c55dec02
dada <zhangda733@gmail.com>
2017-12-27 23:26:43 +0800

    copy antd pro

--- main.js
+++ main.js
@@ -1,107 +1,73 @@
-const path = require('path')
-const glob = require('glob')
 const electron = require('electron')
-const autoUpdater = require('./auto-updater')
-
-const BrowserWindow = electron.BrowserWindow
+// Module to control application life.
 const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
 
-const debug = /--debug/.test(process.argv[2])
+const path = require('path')
+const url = require('url')
 
-if (process.mas) app.setName('Electron APIs')
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
 
-var mainWindow = null
-
-function initialize () {
-  var shouldQuit = makeSingleInstance()
-  if (shouldQuit) return app.quit()
-
-  loadDemos()
-
-  function createWindow () {
+function createWindow () {
+    // Create the browser window.\
     var windowOptions = {
-      width: 1080,
-      minWidth: 680,
-      height: 840,
-      title: app.getName()
-    }
-
-    if (process.platform === 'linux') {
-      windowOptions.icon = path.join(__dirname, '/assets/app-icon/png/512.png')
-    }
-
+        width: 1080,
+        minWidth: 680,
+        height: 840,
+        title: app.getName(),
+        webPreferences: {
+            webSecurity: false
+        }
+      }
     mainWindow = new BrowserWindow(windowOptions)
-    mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
 
-    // Launch fullscreen with DevTools open, usage: npm run debug
-    if (debug) {
-      mainWindow.webContents.openDevTools()
-      mainWindow.maximize()
-      require('devtron').install()
-    }
+    // and load the index.html of the app.
+    // mainWindow.loadURL(url.format({
+    //     pathname: path.join(__dirname, 'index.html'),
+    //     protocol: 'file:',
+    //     slashes: true
+    // }))
+    // mainWindow.loadURL(url.format({
+    //     pathname: path.join(__dirname, './dist/index.html'),
+    //     protocol: 'file:',
+    //     slashes: true }));
+    mainWindow.webContents.openDevTools()
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, './dist/index.html'), protocol: 'file:', slashes: true }))
+    // mainWindow.loadURL("http://localhost:8001/");
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
 
+    // Emitted when the window is closed.
     mainWindow.on('closed', function () {
-      mainWindow = null
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
     })
-  }
+}
 
-  app.on('ready', function () {
-    createWindow()
-    autoUpdater.initialize()
-  })
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
 
-  app.on('window-all-closed', function () {
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-      app.quit()
+        app.quit()
     }
-  })
+})
 
-  app.on('activate', function () {
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
-      createWindow()
+        createWindow()
     }
-  })
-}
-
-// Make this app a single instance app.
-//
-// The main window will be restored and focused instead of a second window
-// opened when a person attempts to launch a second instance.
-//
-// Returns true if the current version of the app should quit instead of
-// launching.
-function makeSingleInstance () {
-  if (process.mas) return false
-
-  return app.makeSingleInstance(function () {
-    if (mainWindow) {
-      if (mainWindow.isMinimized()) mainWindow.restore()
-      mainWindow.focus()
-    }
-  })
-}
-
-// Require each JS file in the main-process dir
-function loadDemos () {
-  var files = glob.sync(path.join(__dirname, 'main-process/**/*.js'))
-  files.forEach(function (file) {
-    require(file)
-  })
-  autoUpdater.updateMenu()
-}
-
-// Handle Squirrel on Windows startup events
-switch (process.argv[1]) {
-  case '--squirrel-install':
-    autoUpdater.createShortcut(function () { app.quit() })
-    break
-  case '--squirrel-uninstall':
-    autoUpdater.removeShortcut(function () { app.quit() })
-    break
-  case '--squirrel-obsolete':
-  case '--squirrel-updated':
-    app.quit()
-    break
-  default:
-    initialize()
-}
+})
